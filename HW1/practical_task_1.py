fe76d35dbd52cf3e3c5031ecbefa13d4fc706cf3
Vyacheslav Anishchenko <avuua16@gmail.com>
2023-05-22 21:03:48 +0200

    Homework HW1 (Python Fundamentals) added

---
+++ HW1/practical_task_1.py
@@ -0,0 +1,8 @@
+name = input('WHAT IS YOUR NAME? ')
+print('HELLO, ' + name)
+
+age = input('HOW OLD ARE YOU? ')
+print('YOUR AGE IS ' + age)
+
+town = input('WHERE DO YOU LIVE? ')
+print('YOU LIVE IN ' + town)
