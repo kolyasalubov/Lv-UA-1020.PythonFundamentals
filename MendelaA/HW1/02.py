d6770b833adf8b0f7fe2689cf7894aa7a796c35d
Andrii Mendela <andrii.mendela@gmail.com>
2023-05-23 09:00:16 +0300

    Home work 1, done

---
+++ MendelaA/HW1/02.py
@@ -0,0 +1,14 @@
+a = int(input('Enter a= '))
+b = int(input('Enter b= '))
+
+print(f'a + b = {a+b}')
+print(f'a - b = {a-b}')
+print(f'a * b = {a*b}')
+print(f'a / b = {a/b}')
+print(f'a ** b = {a**b}')
+print(f'a // b = {a//b}')
+print(f'a % b = {a%b}')
+
+
+
+
