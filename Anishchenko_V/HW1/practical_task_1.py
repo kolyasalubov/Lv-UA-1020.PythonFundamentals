90be8e150753c1d7109e4765b7365d00f086e1e5
Andrii Mendela <andrii.mendela@gmail.com>
2023-05-23 09:12:07 +0300

    Training to del file from my own barnc

--- Anishchenko_V/HW1/practical_task_1.py
+++
@@ -1,8 +0,0 @@
-name = input('WHAT IS YOUR NAME? ')
-print('HELLO, ' + name)
-
-age = input('HOW OLD ARE YOU? ')
-print('YOUR AGE IS ' + age)
-
-town = input('WHERE DO YOU LIVE? ')
-print('YOU LIVE IN ' + town)
