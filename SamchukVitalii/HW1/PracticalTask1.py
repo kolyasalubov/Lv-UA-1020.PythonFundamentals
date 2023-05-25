78f4d66b0695a73926e7bdcd3ac5f7f19c9d72cf
Vitalii_Samchuk <sam.net.code@gmail.com>
2023-05-25 12:31:30 +0300

    add new repository SamchukVitalii

---
+++ SamchukVitalii/HW1/PracticalTask1.py
@@ -0,0 +1,8 @@
+name = str(input('What is your name? - ')) 
+age = str(input('How old are you? - '))
+city = str(input('Where do you live? - '))
+
+
+print("\nHello, " + name,
+      "\nYour age is " + age,
+      "\nYou live in " + city)
