84171b05185ef8aa463150ce4e0c2462a111a825
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-05-26 16:00:27 +0300

    added function check number is numeric in tasck1 HW4

--- OlesiaBarannik/HW4/1.py
+++ OlesiaBarannik/HW4/1.py
@@ -1,14 +1,26 @@
 
-t_Celsius = float(input("Please, Enter the temperature in degrees Celsius:\n"))
+t_Celsius = (input("Please, Enter the temperature in degrees Celsius:\n"))
+
+
+def check_number(t_Celsius):
+        if t_Celsius.isdigit():
+            return True
+        if t_Celsius[0] == '-' and t_Celsius[1:].isdigit():
+            return True
+        if t_Celsius.count('.') == 1:
+            return True
+        else:
+            print("Your temperature should contain only numeric")
+            return False
+
 
 while True:
-    if t_Celsius < -273.15:
+    if check_number(t_Celsius) is False:
+        t_Celsius = (input("Please, Enter the temperature in degrees Celsius:\n"))
+    if float(t_Celsius) < -273.15:
         print("Error: Temperature below absolute zero (-273.15°C)")
-        t_Celsius = float(input("Please, Enter the temperature in degrees Celsius:\n"))
+        t_Celsius = (input("Please, Enter the temperature in degrees Celsius:\n"))
     else:
-        t_Fahrenheit = (t_Celsius * 9 / 5) + 32
+        t_Fahrenheit = (float(t_Celsius) * 9 / 5) + 32
         print(f'{t_Celsius}°C is equivalent to {t_Fahrenheit}°F')
         break
-
-
-
