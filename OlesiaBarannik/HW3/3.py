462f2934b68e125b865a5242b9f1779cb513e569
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-05-26 22:54:49 +0300

    added in function ckeck_number check for the number near the dot

--- OlesiaBarannik/HW3/3.py
+++ OlesiaBarannik/HW3/3.py
@@ -1,13 +1,28 @@
-# Interchange the values of two variables without using the third variable.
+# # Interchange the values of two variables without using the third variable.
+#
+# a = 10
+# b = 12
+# print("Before: ", "a:", a, "b:", b)
+#
+# a, b = b, a
+# print("After: ", "a:", a, "b:", b)
 
-a = 10
-b = 12
-print("Before: ", "a:", a, "b:", b)
+string = "aaa.hhh"
+def is_number(string):
+    if string.isdigit():
+        return True
 
-a, b = b, a
-print("After: ", "a:", a, "b:", b)
+    if string[0] == '-' and string[1:].isdigit():
+        return True
+
+    if string.count('.') == 1:
+        parts = string.split('.')
+        if parts[0].isdigit() and parts[1].isdigit():
+            return True
 
 
+    return False
 
+print(is_number(string))
 
 
