f7a53f39815b5b4846a596f8fbe2fae0f2f0fbac
Maxim Baidak <m7baidak@gmail.com>
2023-05-26 20:06:07 +0300

    some changes in HW3, added hw4_practical into HW4

--- Baidak_Max/HW3/practical_1.py
+++ Baidak_Max/HW3/practical_1.py
@@ -1,13 +1,8 @@
-philosophy_of_python = f"1. Beautiful is better than ugly. \
-                        2. Explicit is better than implicit.\
-                        3. Simple is better than complex.\
-                        4. Complex is better than complicated.\
-                        5. Flat is better than nested.\
-                        6. Sparse is better than dense.\
-                        7. Readability counts.\
-                        8. Special cases aren't special enough to break the rules.\
-                        9. Although practicality beats purity.\
-                        10. Errors should never pass silently."
+philosophy_of_python = "1. Beautiful is better than ugly.\n2. Explicit is better than implicit.\n3. Simple is better " \
+                       "than complex.\n4. Complex is better than complicated.\n5. Flat is better than nested.\n6. " \
+                       "Sparse is better than dense.\n7. Readability counts.\n8. Special cases aren't special enough " \
+                       "to break the rules.\n9. Although practicality beats purity.\n10. Errors should never pass " \
+                       "silently."
 
 words = philosophy_of_python.split()
 better_word = 0
@@ -24,11 +19,12 @@ for word in words:
 print("better word:", better_word)
 print("never word:", never_word)
 print("is word:", is_word)
-
+print(" ")
 
 
 words_upper = philosophy_of_python.upper()
 print(words_upper)
+print(" ")
 
 word_replace = philosophy_of_python.replace("i", "&")
 print(word_replace)
