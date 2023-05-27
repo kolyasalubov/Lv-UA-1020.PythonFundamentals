affbd312509614d6ff3d08d7d8463a3c408b2ad7
Andrii Mendela <andrii.mendela@gmail.com>
2023-05-27 12:13:53 +0300

    Added task 2 to HW5

--- MendelaA/HW5/2.py
+++ MendelaA/HW5/2.py
@@ -1,3 +1,32 @@
 fibon_n = int(input('Input number '))
 
-# 
+lst = [1, 1]
+
+for i in range(2, fibon_n + 1):
+    k = lst[i-1] + lst[i-2]
+       
+    if k > fibon_n:
+        break
+    else:
+        lst.append(k)
+    
+
+print(lst)
+# -----------------------------------------------------------
+
+lst_1 = [1, 1]
+
+x = 0
+i = 1
+while x < fibon_n:
+    i += 1 
+    x = lst_1[i-1] + lst_1[i-2]
+    
+    if x > fibon_n:
+        break
+
+    lst_1.append(x) 
+ 
+    
+  
+print(lst_1)
