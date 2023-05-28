958729abfa5260be694f0eae481eb9ed7facb159
Serhii Buza <buzzaserg@gmail.com>
2023-05-28 15:55:34 +0300

    Added task1 at HW4 folder

---
+++ SerhiiBuza/HW4/task1.py
@@ -0,0 +1,7 @@
+temp_cel = float(input("Enter  temperature"))
+print(temp_cel)
+if (temp_cel >=-273.15):
+    temp_far = ((temp_cel*9/5)+32)
+    print(f"{temp_cel}°C is equivalent to  {temp_far}°K")  
+else:
+    print ("Enter valid value!!! Temperature must be more than -273.15°")
