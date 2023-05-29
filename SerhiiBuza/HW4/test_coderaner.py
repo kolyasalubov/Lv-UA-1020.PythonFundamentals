9947d1500a20a5405809840cf45a6bf3e7095fc8
Serhii Buza <buzzaserg@gmail.com>
2023-05-29 15:32:34 +0300

    aded test fil)

---
+++ SerhiiBuza/HW4/test_coderaner.py
@@ -0,0 +1,11 @@
+# str1 = input("enter")
+# if (str1.isdigit()):
+#     str1=int(str1)
+# elif (str1[0]=="-" and str1[1:].isdigit()):
+#      str1= -int(str1)
+# else:
+#     str1="Not a number"
+# print (str1)    
+a=input("fdf")
+a= -int(a)
+print(a)
