f1a20db3d8e71fe89296a2dbd4a60f0d3a3db6ed
Kostyantyn Tadlya <konstat@ukr.net>
2023-05-30 20:00:19 +0300

    hw5 task2 was added

--- KostyantynTadlya/HW4/Task 1.py
+++ KostyantynTadlya/HW4/Task 1.py
@@ -4,6 +4,3 @@ if Celsius<-273.15:
     print('Temperature below absolute zero %f' %Celsius, chr(176), 'C', sep='')
 else:
     print(f"{Celsius}", chr(176), f"C is equivalent to {Celsius*9/5+32}", chr(176), "F", sep='')
-
-    
-
