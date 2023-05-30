f95446f91f9b7068c54877fafd180016ffa11f8a
Yuliia Shaposhnikova <domik339@gmail.com>
2023-05-30 18:30:54 +0300

    if_elif_else edded in Task2 in HW5

--- YuliiaShaposhnikova/HW5/Task2.py
+++ YuliiaShaposhnikova/HW5/Task2.py
@@ -1,6 +1,11 @@
 n = int(input('Enter the end number of the Fibonacci numbers: '))
-fibonacci_numbers = [0, 1]
-for i in range(2, n):
-    next_number = fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2]
-    fibonacci_numbers.append(next_number)
-print(fibonacci_numbers)
+if n == 0:
+    print([])
+elif n == 1:
+    print(0)
+else:
+    fibonacci_numbers = [0, 1]
+    for i in range(2, n):
+        next_number = fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2]
+        fibonacci_numbers.append(next_number)
+    print(fibonacci_numbers)
