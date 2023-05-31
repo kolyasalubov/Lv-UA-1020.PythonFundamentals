0fa6d31d4a216aafef8394f4f0cfb1c0fcf67e65
Andrii Mendela <andrii.mendela@gmail.com>
2023-05-31 17:02:11 +0300

    Fix range

--- MendelaA/HW6/1.py
+++ MendelaA/HW6/1.py
@@ -7,16 +7,14 @@ devided_by_2 = []
 devided_by_3 = []
 not_devided_by_2_and_3 = []
 
-for item in range(11):
-    #print(item)
+for item in range(1, 11):
     if item % 2 == 0:
         devided_by_2.append(item)
     elif item % 3 == 0:
         devided_by_3.append(item)
     else:
         not_devided_by_2_and_3.append(item)
-    
-
+  
 print(f'Divided by 2 {devided_by_2}\n'
       f'Divided by 3 {devided_by_3}\n'
       f'Diveded by 2 and 3 {not_devided_by_2_and_3}')
