0308a7126cd766508b018bcfd48e7102a4fe9465
Mariia Novytska <mnovickaa@gmail.com>
2023-05-31 12:01:45 +0300

    Update HW2. Used slice to write number in reverse order

--- MariiaNovitska/HW2/natural number.py
+++ MariiaNovitska/HW2/natural number.py
@@ -1,12 +1,13 @@
 input_number = input("Write your number: ")
 
 a = list(input_number)
-product = int(a[0])*int(a[1])*int(a[2])*int(a[3])
-b = a[::-1]
-reverse_number = int(''.join(map(str, b))) 
+product = int(a[0])*int(a[1])*int(a[2])*int(a[3]) 
 c = sorted(a)
 ascending = int(''.join(map(str, c)))
 
 print("The product of the digits of this number = ", product)
-print("The number in reverse order = ", reverse_number)
+print(f"The number in reverse order = {input_number[::-1]}")
 print("Numbers in ascending order = ", ascending)
+
+
+
