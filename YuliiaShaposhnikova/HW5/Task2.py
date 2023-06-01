800f4d5f95d837ebc977e439ea209e2f5b7a000e
Yuliia Shaposhnikova <domik339@gmail.com>
2023-06-01 12:16:16 +0300

    remarks fixed in Task2 in HW5

--- YuliiaShaposhnikova/HW5/Task2.py
+++ YuliiaShaposhnikova/HW5/Task2.py
@@ -1,11 +1,11 @@
 n = int(input('Enter the end number of the Fibonacci numbers: '))
 if n == 0:
-    print([])
+    print('fibonacci_numbers = []')
 elif n == 1:
-    print(0)
+    print('fibonacci_numbers = [0]')
 else:
     fibonacci_numbers = [0, 1]
     for i in range(2, n):
         next_number = fibonacci_numbers[i - 1] + fibonacci_numbers[i - 2]
         fibonacci_numbers.append(next_number)
-    print(fibonacci_numbers)
+    print(f'fibonacci_numbers = {fibonacci_numbers}')
