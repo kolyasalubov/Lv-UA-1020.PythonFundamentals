80a369db70759c5f7c966f3deb74bac3803f67a9
Yuliia Shaposhnikova <domik339@gmail.com>
2023-06-02 17:04:34 +0300

    Added clarification according to the requirements

--- YuliiaShaposhnikova/HW6/Task1.py
+++ YuliiaShaposhnikova/HW6/Task1.py
@@ -5,7 +5,7 @@ not_div_2_3 = []
 for item in range(1, 11):
     if item % 2 == 0:
         div_2.append(item)
-    if item % 3 == 0:
+    if item % 3 == 0 and item % 2 != 0:
         div_3.append(item)
     if item % 2 != 0 and item % 3 != 0:   # or if item % 2 == 1 and item % 3 == 1:
         not_div_2_3.append(item)
