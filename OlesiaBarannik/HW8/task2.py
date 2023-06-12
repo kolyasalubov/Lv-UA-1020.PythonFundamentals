dea341b3895b8282deb448f9ddfcb411acf1aaf7
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-06-12 14:43:35 +0300

    added task2 into HW8

---
+++ OlesiaBarannik/HW8/task2.py
@@ -0,0 +1,52 @@
+
+user_password = input("Please, Enter password :\n")
+spec_char = ["$", "#", "@"]
+
+def valid_len(user_password):
+    if len(user_password) > 6 and len(user_password) < 16:
+        return True
+    return False
+
+def valid_number(user_password):
+    for i in user_password:
+        if i.isdigit():
+            return True
+    return False
+
+def valid_upper(user_password):
+    for i in user_password:
+        if i >= 'A' and i <= 'Z':
+            return True
+    return False
+
+def valid_lower(user_password):
+    for i in user_password:
+        if i >= 'a' and i <= 'z':
+            return True
+    return False
+
+def valid_spec_char(user_password):
+    for i in spec_char:
+        if i in user_password:
+            return True
+    return False
+
+while True:
+    if valid_len(user_password) is False:
+        print("Error: Password must contain between 6 and 16 characters")
+        user_password = (input("Please, Enter password :\n"))
+    elif valid_number(user_password) is False:
+        print("Error: Password must contain numbers")
+        user_password = (input("Please, Enter password :\n"))
+    elif valid_upper(user_password) is False:
+        print("Error: Password must contain an uppercase letter")
+        user_password = (input("Please, Enter password :\n"))
+    elif valid_lower(user_password) is False:
+        print("Error: Password must contain a lowercase letter")
+        user_password = (input("Please, Enter password :\n"))
+    elif valid_spec_char(user_password) is False:
+        print("Error: Password must contain a special character ($,#,@)")
+        user_password = (input("Please, Enter password :\n"))
+    else:
+        print(f'This is your password {user_password}')
+        break
