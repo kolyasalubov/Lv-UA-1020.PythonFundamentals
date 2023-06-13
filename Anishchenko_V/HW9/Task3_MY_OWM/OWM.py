f643120bccd5d46c9d2f5275f1f4d02260f2e756
Vyacheslav Anishchenko <avuua16@gmail.com>
2023-06-13 15:40:26 +0200

    additions to HW9

---
+++ Anishchenko_V/HW9/Task3_MY_OWM/OWM.py
@@ -0,0 +1,31 @@
+from pyowm import OWM
+
+
+API_KEY = '12d77c99f4c4d0f1ad8c836aa1c3ff45'
+# ---------- FREE API KEY examples ---------------------
+
+owm = OWM(API_KEY)
+mgr = owm.weather_manager()
+
+
+def weather(location):
+
+    # get weather data from OWM
+    observation = mgr.weather_at_place(location)
+    w = observation.weather
+
+    # 'translate' wind direction to human-friendle representation
+    directions = ('North', 'Nord-East', 'East', 'South-East',
+                  'South', 'South-West', 'West', 'North-West')
+    wind_deg = w.wind()['deg']
+    marker = 23
+    for i in range(0, 9):
+        if wind_deg < marker + 45 * i:
+            wind_direction = directions[i]
+            break
+
+    # get dict of temperatures
+    temperat = w.temperature("celsius")
+
+    # return string to be printed in GUI's lower frame
+    return f'Description: {w.detailed_status}\n\nWind:\n    speed: {w.wind()["speed"]} m/s\n    direction: {wind_direction}\n\nHumidity: {w.humidity}%\n\nTemperature (Celcius):\n    current: {temperat["temp"]}\n    feels like: {temperat["feels_like"]}\n    maximum: {temperat["temp_max"]}\n    minimum: {temperat["temp_min"]}'
