a65f762413a1b4a4e7ec7943af3b0ad34e86da46
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-06-15 01:17:57 +0300

    added task1 in dir HW9

---
+++ OlesiaBarannik/HW9/task1.py
@@ -0,0 +1,20 @@
+import random
+
+x = random.randint(1,100)
+print(x)
+user_x = int(input("Enter your number:\n"))
+
+counter = 0
+while counter <= 10:
+  if counter == 10:
+    print("You only have 10 attempts, try again")
+  elif user_x > x:
+    print("Your number is greater")
+    user_x = int(input())
+  elif user_x < x:
+    print("Your number is less")
+    user_x = int(input())
+  else:
+    print("Congratulations, you are the winner")
+    break
+  counter += 1
