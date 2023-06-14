ba32f613bf952acdd550bfd7d194d1fb015ae534
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-06-15 01:22:44 +0300

    corect task1 HW9

--- OlesiaBarannik/HW9/task1.py
+++ OlesiaBarannik/HW9/task1.py
@@ -10,10 +10,10 @@ while counter <= 10:
     print("You only have 10 attempts, try again")
   elif user_x > x:
     print("Your number is greater")
-    user_x = int(input())
+    user_x = int(input("Enter your number:\n"))
   elif user_x < x:
     print("Your number is less")
-    user_x = int(input())
+    user_x = int(input("Enter your number:\n"))
   else:
     print("Congratulations, you are the winner")
     break
