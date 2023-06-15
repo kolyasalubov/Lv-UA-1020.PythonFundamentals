1e4e35c0f829f9e31abd8403fa6a5c9d60c56625
Andy Dorokhin <35396894+AndyDorokhin@users.noreply.github.com>
2023-06-15 09:53:58 +0300

    Module 8 Homework HW8 (task 3 fixed)

--- AndyDorokhin/HW8/task3.py
+++ AndyDorokhin/HW8/task3.py
@@ -8,3 +8,26 @@
     three functions for finding areas, into the main program. The basic logic of the
     program is executed in the main module).
 """
+from task3_helper import rectangle_area, triangle_area, circle_area
+
+def main():
+    print("1. Rectangle")
+    print("2. Triangle")
+    print("3. Circle")
+    choice = input("Enter your choice: ").strip()
+    if choice == '1':
+        a = float(input("Enter a: "))
+        b = float(input("Enter b: "))
+        print(f"Area of rectangle: {rectangle_area(a, b):.2f}")
+    elif choice == '2':
+        base = float(input("Enter base: "))
+        height = float(input("Enter height: "))
+        print(f"Area of triangle: {triangle_area(base, height):.2f}")
+    elif choice == '3':
+        radius = float(input("Enter radius: "))
+        print(f"Area of circle: {circle_area(radius):.2f}")
+    else:
+        print("Invalid choice")
+    
+if __name__ == "__main__":
+    main()
