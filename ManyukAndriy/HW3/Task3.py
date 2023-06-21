5525cdd75c0799686ebbfe4de6a40385e0aa8831
Andriy Manyuk <andriy.manyuk@gmail.com>
2023-06-21 23:05:51 +0200

    edited task3

--- ManyukAndriy/HW3/Task3.py
+++ ManyukAndriy/HW3/Task3.py
@@ -1,7 +1,14 @@
 variable1 = "softserve.academy"
-Variable2 = "w3schools.com/python"
+variable2 = "w3schools.com/python"
+print(variable1, variable2)
 
 #changing a new variable to variable1 and variable2
 
-variable1 = "softserve.com"
-Variable2 = "google.com"
+variable1, variable2 = variable2, variable1
+print(variable1, variable2)
+# a = 5
+# b = 15
+# # c = a; a = b; b = c;
+# print (a, b)
+# a, b = b, a
+# print(a, b)
