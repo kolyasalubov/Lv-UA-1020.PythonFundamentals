a64b181a95ceb9c80de16ca95a870db1665341ce
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-06-21 23:45:58 +0300

    added task5 codewars

--- OlesiaBarannik/HW10/codewars_OOP.py
+++ OlesiaBarannik/HW10/codewars_OOP.py
@@ -56,3 +56,27 @@
 # a1 = Person("John", 25)
 # a1.get()
 ##################################################################################################
+# Now that we have a Block let's move on to something slightly more complex: a Sphere.
+# import math
+# class Sphere(object):
+#     def __init__(self, radius, mass):
+#         self.radius = radius
+#         self.mass = mass
+#     def get_radius(self):
+#         return self.radius
+#     def get_mass(self):
+#         return self.mass
+#     def get_volume(self):
+#         return round((4 * math.pi * self.radius**3)/3, 5)
+#     def get_surface_area(self):
+#         return round((4 * math.pi * self.radius**2), 5)
+#     def get_density(self):
+#         return round((self.mass/(4/3 * math.pi * self.radius**3)), 5)
+#
+# ball = Sphere(2,50)
+# ball.get_radius()
+# ball.get_mass()
+# ball.get_volume()
+# ball.get_surface_area()
+# ball.get_density()
+##################################################################################################
