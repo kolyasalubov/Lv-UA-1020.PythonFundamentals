5eca90f18d484d4059f77e05085076cf6492b90b
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-06-21 22:38:51 +0300

    added task3 codewars

--- OlesiaBarannik/HW10/codewars_OOP.py
+++ OlesiaBarannik/HW10/codewars_OOP.py
@@ -23,5 +23,19 @@
 # ghost = Ghost()
 # print(ghost.color)
 ##################################################################################################
+# You have to do God's job. The creation method must return an array of length 2 containing objects
+# (representing Adam and Eve). The first object in the array should be an instance of the class Man.
+# The second should be an instance of the class Woman. Both objects have to be subclasses of Human.
+# Your job is to implement the Human, Man and Woman classes.
 
-
+# class Human:
+#     pass
+# class Man(Human):
+#     pass
+# class Woman(Human):
+#     pass
+#
+# def God():
+#     Adam = Man()
+#     Eve = Woman()
+#     return [Adam, Eve]
