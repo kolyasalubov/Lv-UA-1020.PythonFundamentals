64beabd9f06cdd005a7a20eaac2d1de5d7f26213
Olesia Barannik <OlesyaBarannik23@gmail.com>
2023-06-21 23:05:41 +0300

    added task4 codewars

--- OlesiaBarannik/HW10/codewars_OOP.py
+++ OlesiaBarannik/HW10/codewars_OOP.py
@@ -39,3 +39,20 @@
 #     Adam = Man()
 #     Eve = Woman()
 #     return [Adam, Eve]
+##################################################################################################
+# Your task is to complete this Class, the Person class has been created. You must fill in the Constructor
+# method to accept a name as string and an age as number, complete the get
+# Info property and getInfo method/Info getter which should return johns age is 34
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#         self.info = f"{self.name}s age is {self.age}"
+#
+#     def info(self):
+#         return self.info
+#
+# a1 = Person("John", 25)
+# a1.get()
+##################################################################################################
