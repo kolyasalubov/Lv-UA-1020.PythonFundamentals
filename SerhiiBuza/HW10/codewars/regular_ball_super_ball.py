f658e40c33f09b9d65d8115d219ac5a6063a4c6a
Serhii Buza <buzzaserg@gmail.com>
2023-06-25 21:42:15 +0300

    HW10 Tasks codewars is Done

---
+++ SerhiiBuza/HW10/codewars/regular_ball_super_ball.py
@@ -0,0 +1,8 @@
+""".git\Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+"""
+class Ball ():
+    def __init__(self, ball_type= "regular"):
+        self.ball_type = ball_type
+
